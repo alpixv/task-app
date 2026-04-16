3fef9b87a64ddb032050e7e07efdbe58da11b924
Alp Arslan Begenjov <begenjowalparslan3@gmail.com>
2026-04-17 04:04:17 +0800

    Add initial task app files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # task-app
 A clean and simple task app to manage daily productivity.
+/Users/alpixv/Documents/GitHub/task-app/Task_app(AlparslanBegenjov)
---
+++ Task_app(AlparslanBegenjov)/app/src/main/java/com/example/task_appalparslanbegenjov/OnDialogCloseListener.java
@@ -0,0 +1,7 @@
+package com.example.task_appalparslanbegenjov;
+
+import android.content.DialogInterface;
+
+public interface OnDialogCloseListener {
+    void onDialogClose(DialogInterface dialogInterface);
+}
